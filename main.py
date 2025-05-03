bd0ddd4757e408bce27abbf2295fd901a53ba37d
cracery <154086692+cracery@users.noreply.github.com>
2025-05-03 18:34:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from deepface import DeepFace
 from model_loader import load_weighted_model
 
 stress_model = None
-model_metadata = None
-ftr_importance = None
+model_metad = None
+ftr_imprtnce = None
 learned_wghts = None
 
 app = FastAPI(title="Stress Detection API", description="Facial expression stress recognition API.")
@@ -43,39 +43,34 @@ except RuntimeError:
 
 @app.get("/", response_class=HTMLResponse)
 async def serve_root():
-    """
-    returns sld.html when goes to root "/"
-    """
     return FileResponse("static/sld.html")
+
 
 @app.get("/sld", response_class=HTMLResponse)
-async def serve_speedometer():
+async def serve_sld():
     return FileResponse("static/sld.html")
 
-def default_html_response():
-    """
-    returns default, if file not found
-    """
+def default_html_respo():
     return FileResponse("static/Default.html")
-
 
 
 # check if Deepface works properly
 try:
     dummy_img = np.zeros((100, 100, 3), dtype=np.uint8)
-    dummy_result = DeepFace.analyze(img_path=dummy_img, actions=['emotion'], enforce_detection=False)
+    dummy_result = DeepFace.analyze(img_path=dummy_img, actions=['emotion'], enforce_detect=False)
     print("DeepFace initialised")
 except Exception as e:
     print(f"Error while initialize DeepFace: {str(e)}")
     import traceback
     print(traceback.format_exc())
 
+
 @app.on_event("startup")
 async def startup_event():
-    global model_name, stress_model, model_metadata, ftr_importance, learned_wghts
+    global model_name, stress_model, model_metad, ftr_imprtnce, learned_wghts
     try:
         print("loading model while starting...")
-        stress_model, ftr_importance, learned_wghts, model_metadata = load_weighted_model(model_name)
+        stress_model, ftr_imprtnce, learned_wghts, model_metad = load_weighted_model(model_name)
         print("Model loaded successfuly")
     except Exception as e:
         print(f"Error while loading model: {str(e)}")
@@ -92,6 +87,7 @@ async def startup_event():
             print(f"Model DeepFace was found by path {model_path}")
         else:
             print(f"Model DeepFace was not found by path {model_path}")
+            
             # Alternative way
             if os.path.isfile("/root/.deepface/weights/efacial_expression_model_weights.h5"):
                 print("Model was found by an alternative path")
@@ -101,14 +97,14 @@ async def startup_event():
         print(f"Error while checking DeepFace: {e}")
 
 class Emot_Vector(BaseModel):
-    emotions: List[float]
+    emots: List[float]
 
 class Stress_respo(BaseModel):
     stress_level: str
-    probabilities: dict
+    probs: dict
     confidence: float
 
-def generate_emotion_vector(img_data):
+def gener_emot_vector(img_data):
     """
     Generate emotions vector with DeepFace model.
     """
@@ -118,18 +114,18 @@ def generate_emotion_vector(img_data):
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
         try:
-            predictions = DeepFace.analyze(img, actions=['emotion'], enforce_detection=False)
+            predictions = DeepFace.analyze(img, actions=['emotion'], enforce_detect=False)
 
             if isinstance(predictions, list):
                 predictions = predictions[0]
 
-            emotions = predictions["emotion"]
+            emots = predictions["emotion"]
 
 
             all_emot = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral', 'contempt']
 
             # Create vector with all emotions
-            emot_vector = [emotions.get(emotion, 0.0) for emotion in all_emot]
+            emot_vector = [emots.get(emotion, 0.0) for emotion in all_emot]
             emot_vector = np.array(emot_vector, dtype=np.float32)
 
             # noramlise
@@ -139,24 +135,24 @@ def generate_emotion_vector(img_data):
         
 
         except Exception as e:
-            print(f"Помилка аналізу емоцій: {e}")
+            print(f"Emotional analysis error: {e}")
             default_vector = np.array([0.05, 0.05, 0.05, 0.1, 0.05, 0.05, 0.6, 0.05], dtype=np.float32)
             return default_vector
     except Exception as e:
         print(f"Critical error while proccesing image: {e}")
         raise HTTPException(status_code=400, detail=f"error while proccesing image: {str(e)}")
 
-def calibrated_predict_with_model(emotion_vector):
+def calibrated_predict_with_model(emot_vector):
     """
     Make stress level prediction besed on emotions vector.(lvls + probs)
     """
-    global model_name, stress_model, model_metadata, ftr_importance, learned_wghts
+    global model_name, stress_model, model_metad, ftr_imprtnce, learned_wghts
     
     if stress_model is None:
-        raise HTTPException(status_code=500, detail="Модель не завантажена")
+        raise HTTPException(status_code=500, detail="Model is not loaded")
     
     stress_model.eval()
-    input_tensor = torch.tensor(emotion_vector, dtype=torch.float32).unsqueeze(0)
+    input_tensor = torch.tensor(emot_vector, dtype=torch.float32).unsqueeze(0)
 
     with torch.no_grad():
         outputs = stress_model(input_tensor)
@@ -167,17 +163,17 @@ def calibrated_predict_with_model(emotion_vector):
     pred_index = np.argmax(probs)
     pred_class = stress_levels[pred_index]
     
+
     # Get stress class metrics
     class_metrics = {}
-    if model_metadata and 'metrics' in model_metadata:
-        metrics = model_metadata['metrics']
+    if model_metad and 'metrics' in model_metad:
+        metrics = model_metad['metrics']
         for metric_name in ['precision', 'recall', 'f1_score']:
             if metric_name in metrics and pred_class in metrics[metric_name]:
                 class_metrics[metric_name] = metrics[metric_name][pred_class]
     
-
     prob_dict = {level: float(prob) for level, prob in zip(stress_levels, probs)}
-    return pred_class, prob_dict, float(probs[pred_index]), class_metrics
+    return pred_class,prob_dict, float(probs[pred_index]),class_metrics
 
 class Stress_respo(BaseModel):
     stress_level: str
@@ -188,22 +184,20 @@ class Stress_respo(BaseModel):
 
 # Preprocess iamge
 def resize_img(img, max_size=800):
-    wdth, height = img.size
+    wdth, hght = img.size
     
-    if wdth > max_size or height > max_size:
-        if wdth > height:
+    if wdth > max_size or hght > max_size:
+        if wdth > hght:
             new_wdth = max_size
-            new_height = int(height * (max_size / wdth))
+            new_hght = int(hght * (max_size / wdth))
         else:
-            new_height = max_size
-            new_wdth = int(wdth * (max_size / height))
+            new_hght = max_size
+            new_wdth = int(wdth * (max_size / hght))
         
-
-        resized_img = img.resize((new_wdth, new_height), Image.LANCZOS)
+        resized_img = img.resize((new_wdth, new_hght), Image.LANCZOS)
         return resized_img
     
     return img
-
 
 
 @app.post("/predict/image")
@@ -212,11 +206,11 @@ async def predict_image(file: UploadFile = File(...)):
         # Add request info into logs
         print(f"Request was got: {file.filename}, content_type: {file.content_type}")
         
-        global model_name, stress_model, model_metadata, ftr_importance, learned_wghts
+        global model_name, stress_model, model_metad, ftr_imprtnce, learned_wghts
         
         if stress_model is None:
             print("Loading stress model...")
-            stress_model, ftr_importance, learned_wghts, model_metadata = load_weighted_model(model_name)
+            stress_model, ftr_imprtnce, learned_wghts, model_metad = load_weighted_model(model_name)
         
         # Read img
         contents = await file.read()
@@ -234,8 +228,8 @@ async def predict_image(file: UploadFile = File(...)):
             print(f"Image successfully decoded, size: {img.size}, format: {img.format}")
 
 
-            max_image_size = 800 
-            img = resize_img(img, max_image_size)
+            max_img_size = 800 
+            img = resize_img(img, max_img_size)
             print(f"Image size after processing: {img.size}")
             
 
@@ -251,18 +245,18 @@ async def predict_image(file: UploadFile = File(...)):
             
             # Process exceptional cases
             try:
-                emots_result = DeepFace.analyze(img_path=img_array, actions=['emotion'], enforce_detection=False)
+                emots_result = DeepFace.analyze(img_path=img_array, actions=['emotion'], enforce_detect=False)
                 print(f"DeepFace process image successfuly")
             except Exception as deepface_error:
                 print(f"Error on the first try to analyse DeepFace: {str(deepface_error)}")
                 
                 try:
                     gray = cv2.cvtColor(img_array, cv2.COLOR_RGB2GRAY)
-                    normalized = cv2.equalizeHist(gray)
-                    enhanced = cv2.cvtColor(normalized, cv2.COLOR_GRAY2RGB)
+                    normalised = cv2.equalizeHist(gray)
+                    enhanced = cv2.cvtColor(normalised, cv2.COLOR_GRAY2RGB)
                     
                     print("Image enhanced for face recognition")
-                    emots_result = DeepFace.analyze(img_path=enhanced, actions=['emotion'], enforce_detection=False)
+                    emots_result = DeepFace.analyze(img_path=enhanced, actions=['emotion'], enforce_detect=False)
                     print("DeepFace successfully analyses enhanced image")
                 except Exception as enhanced_error:
                     print(f"Error when analysing an enhanced image: {str(enhanced_error)}")
@@ -293,10 +287,8 @@ async def predict_image(file: UploadFile = File(...)):
             if sum_values > 0:
                 input_values = [v / sum_values for v in input_values]
             
-
             print("Call the stress analysis function...")
             
-
             input_tensor = torch.tensor([input_values], dtype=torch.float32)
             
             with torch.no_grad():
@@ -307,21 +299,20 @@ async def predict_image(file: UploadFile = File(...)):
             stress_result = {stress_classes[i]: float(stress_probs[i]) for i in range(len(stress_classes))}
             
 
-            predicted_stress = stress_classes[np.argmax(stress_probs)]
+            pred_stress = stress_classes[np.argmax(stress_probs)]
             
-
             result = {
                 "emotions": emots,
                 "stress_probabilities": stress_result,
-                "predicted_stress": predicted_stress
+                "predicted_stress": pred_stress
             }
             
-            print(f"Stress analysis result: {predicted_stress}")
+            print(f"Stress analysis result: {pred_stress}")
             return {"status": "success", "result": result}
             
-        except Exception as image_error:
-            print(f"Image processing error: {str(image_error)}")
-            return {"status": "error", "detail": f"Image processing error: {str(image_error)}"}
+        except Exception as img_error:
+            print(f"Image processing error: {str(img_error)}")
+            return {"status": "error", "detail": f"Image processing error: {str(img_error)}"}
             
     except Exception as e:
         print(f"General image analysis error: {str(e)}")
@@ -332,5 +323,5 @@ async def predict_image(file: UploadFile = File(...)):
 
 @app.get("/healthcheck")
 async def healthcheck():
-    global model_name, stress_model, model_metadata
+    global model_name, stress_model, model_metad
     return {"status": "ok", "model_loaded": stress_model is not None}
