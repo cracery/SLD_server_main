a2d7558f55e2d1f6cc8519ad0587e7b496ec17f1
cracery <154086692+cracery@users.noreply.github.com>
2025-05-09 15:41:29 +0300

    Update camera.js

--- static/camera.js
+++ static/camera.js
@@ -16,18 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
   captureBtn.addEventListener('click', () => {
+    console.log("Camera script loaded");
     const ctx = canvas.getContext('2d');
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
     canvas.toBlob(blob => {
+      console.log("Capturing image from video...");
       const file = new File([blob], 'captured.jpg', { type: 'image/jpeg' });
-
+      console.log("Created blob:", blob);
       preview.src = URL.createObjectURL(file);
       preview.style.display = 'block';
 
       // Зберігаємо файл у preview для подальшої обробки
       preview.fileBlob = file;
       preview.dataset.fromCamera = "true";
+      console.log("Saved to preview:", file);
     }, 'image/jpeg');
   });
 });
