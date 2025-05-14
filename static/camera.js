774f14e6108a634968b863a116fab61d06379b0c
cracery <154086692+cracery@users.noreply.github.com>
2025-05-14 17:10:25 +0300

    Update camera.js

--- static/camera.js
+++ static/camera.js
@@ -1,45 +1,41 @@
-let videoStream;
-document.addEventListener('DOMContentLoaded', () => {
-  const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas');
-  const preview = document.getElementById('image-preview');
-  const captureBtn = document.getElementById('capture-btn');
-
+var videoStream;
+document.addEventListener('DOMContentLoaded',function() {
+  var video= document.getElementById('video');
+  var canvas=document.getElementById('canvas');
+  var preview=document.getElementById('image-preview');
+  var captureBtn=document.getElementById('capture-btn');
   if (!video || !canvas || !captureBtn || !preview) return;
-  navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-      videoStream = stream;
-      video.srcObject = stream;
+  navigator.mediaDevices.getUserMedia({video:true})
+    .then(function(stream) {
+      videoStream= stream;
+      video.srcObject=stream;
     })
-    .catch(err => {
-      alert('Cannot access camera: ' + err.message);
+    .catch(function(err){
+      alert('Cannot access camera: '+ err.message);
     });
-
-  captureBtn.addEventListener('click', () => {
+  captureBtn.addEventListener('click',function() {
     console.log("Camera script loaded");
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;    
-    const ctx = canvas.getContext('2d');
+    canvas.width= video.videoWidth;
+    canvas.height= video.videoHeight;    
+    var ctx= canvas.getContext('2d');
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-
-    canvas.toBlob(blob => {
+    canvas.toBlob(function(blob) {
       console.log("Capturing image from video...");
-      const file = new File([blob], 'captured.jpg', { type: 'image/jpeg' });
-      console.log("Created blob:", blob);
-      preview.src = URL.createObjectURL(file);
-      preview.style.display = 'block';
-
+      var file= new File([blob],'captured.jpg',{ type: 'image/jpeg' });
+      console.log("Created blob:",blob);
+      preview.src= URL.createObjectURL(file);
+      preview.style.display= 'block';
       // save to preview
-      preview.fileBlob = file;
+      preview.fileBlob= file;
       preview.dataset.fromCamera = "true";
       console.log("Saved to preview:", file);
     }, 'image/jpeg');
   });
 });
-
-window.addEventListener('beforeunload', () => {
+window.addEventListener('beforeunload',function(){
   if (videoStream) {
-    videoStream.getTracks().forEach(track => track.stop());
+    videoStream.getTracks().forEach(function(track){
+      track.stop();
+    });
   }
 });
